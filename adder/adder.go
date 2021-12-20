697faf987062e253b14dba0c9ba24b053aaa687c
Viktor Hryhorchuk <viktor_hryhorchuk@epam.com>
2021-12-20 18:07:24 +0200

    Fix slice cap.

--- adder/adder.go
+++ adder/adder.go
@@ -4,3 +4,22 @@ package adder
 func Add(first int, second int) (int, error) {
 	return first + second, nil
 }
+
+func Sum(numbers[] int) int {
+	res := 0
+	for _, num := range(numbers) {
+		res += num
+	}
+	return res
+}
+
+func AddAll(nums...[]int) (summed []int) {
+	// sum := make([]int, len(nums)) // not good as we may need slice with bigger capacity
+	var sums []int
+
+	for _, num := range nums {
+		sums = append(sums, Sum(num))
+	}
+
+	return sums
+}
--- adder/adder_test.go
+++ adder/adder_test.go
@@ -1,6 +1,7 @@
 package adder_test
 
 import (
+	"reflect"
 	"fmt"
 	"github.com/vicgrygorchyk/example_hello/adder"
 	"testing"
@@ -17,6 +18,32 @@ func TestAdd(t *testing.T) {
 
 		if res != expected {
 			t.Errorf("expected %d, got %d", res, expected)
+		}
+	})
+}
+
+func TestAddAll(t *testing.T) {
+	t.Run("Check AddAll with two param", func (t *testing.T) {
+		res := adder.AddAll([]int{1, 2}, []int{5, 6})
+		expected := []int {3, 11}
+		if !reflect.DeepEqual(res, expected) {
+			t.Errorf("Expected %d, got %d", res, expected)
+		}
+	})
+
+	t.Run("Check AddAll with three param", func (t *testing.T) {
+		res := adder.AddAll([]int{1, 2}, []int{5, 6}, []int{2, 2, 2})
+		expected := []int {3, 11, 6}
+		if !reflect.DeepEqual(res, expected) {
+			t.Errorf("Expected %d, got %d", res, expected)
+		}
+	})
+
+	t.Run("Check AddAll with big slices", func (t *testing.T) {
+		res := adder.AddAll([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, []int{5, 6}, []int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2})
+		expected := []int {66, 11, 20}
+		if !reflect.DeepEqual(res, expected) {
+			t.Errorf("Expected %d, got %d", res, expected)
 		}
 	})
 }
